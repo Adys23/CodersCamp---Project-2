bc88a31a89e825b4bdf9a6263ef9fea85c93fa68
Adrian Wasilewski <wasilewski.adrian.64@gmail.com>
2019-08-20 09:02:50 +0200

    grid on buttons in EmpOverview

--- time_tracking_tool/src/EmployeeOverview.js
+++ time_tracking_tool/src/EmployeeOverview.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Dropdown from 'react-bootstrap/Dropdown'
 import DropdownButton from 'react-bootstrap/DropdownButton'
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 class EmployeeOverview extends React.Component {
     render() {
@@ -9,31 +9,30 @@ class EmployeeOverview extends React.Component {
             <div>
                 <Container>
                     <Row>
-                        <DropdownButton id="dropdown-basic-button" title="Select year">
-                            <Dropdown.Item href="#/action-1">2019</Dropdown.Item>
-                            <Dropdown.Item href="#/action-2">2020</Dropdown.Item>
-                            <Dropdown.Item href="#/action-3">2021</Dropdown.Item>
-                        </DropdownButton>
-                        <DropdownButton id="dropdown-basic-button" title="Select month">
-                            <Dropdown.Item href="#/action-1">January</Dropdown.Item>
-                            <Dropdown.Item href="#/action-2">February</Dropdown.Item>
-                            <Dropdown.Item href="#/action-3">March</Dropdown.Item>
-                            <Dropdown.Item href="#/action-4">April</Dropdown.Item>
-                            <Dropdown.Item href="#/action-5">May</Dropdown.Item>
-                            <Dropdown.Item href="#/action-6">June</Dropdown.Item>
-                            <Dropdown.Item href="#/action-7">July</Dropdown.Item>
-                            <Dropdown.Item href="#/action-8">August</Dropdown.Item>
-                            <Dropdown.Item href="#/action-9">September</Dropdown.Item>
-                            <Dropdown.Item href="#/action-10">October</Dropdown.Item>
-                            <Dropdown.Item href="#/action-11">November</Dropdown.Item>
-                            <Dropdown.Item href="#/action-12">December</Dropdown.Item>
-                        </DropdownButton>
+                        <Col>
+                            <DropdownButton id="dropdown-basic-button" title="Select year">
+                                <Dropdown.Item href="#/action-1">2019</Dropdown.Item>
+                                <Dropdown.Item href="#/action-2">2020</Dropdown.Item>
+                                <Dropdown.Item href="#/action-3">2021</Dropdown.Item>
+                            </DropdownButton>
+                        </Col>
+                        <Col>
+                            <DropdownButton id="dropdown-basic-button" title="Select month">
+                                <Dropdown.Item href="#/action-1">January</Dropdown.Item>
+                                <Dropdown.Item href="#/action-2">February</Dropdown.Item>
+                                <Dropdown.Item href="#/action-3">March</Dropdown.Item>
+                                <Dropdown.Item href="#/action-4">April</Dropdown.Item>
+                                <Dropdown.Item href="#/action-5">May</Dropdown.Item>
+                                <Dropdown.Item href="#/action-6">June</Dropdown.Item>
+                                <Dropdown.Item href="#/action-7">July</Dropdown.Item>
+                                <Dropdown.Item href="#/action-8">August</Dropdown.Item>
+                                <Dropdown.Item href="#/action-9">September</Dropdown.Item>
+                                <Dropdown.Item href="#/action-10">October</Dropdown.Item>
+                                <Dropdown.Item href="#/action-11">November</Dropdown.Item>
+                                <Dropdown.Item href="#/action-12">December</Dropdown.Item>
+                            </DropdownButton>
+                        </Col>
                     </Row>
-                </Container>
-            </div>
-            <div>
-                <Container>
-                    
                 </Container>
             </div>
         )
